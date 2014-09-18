218dfe3c3f6f3525b20d2a2a1fb03cd57d196364
silkbottle <silkbottle@gmail.com>
2014-09-18 18:01:29 +0400

    some fixes with video gap

--- src/handlers/hls_writer.erl
+++ src/handlers/hls_writer.erl
@@ -15,8 +15,9 @@
   data :: binary(),
   pts :: non_neg_integer(),
   dts :: non_neg_integer(),
-  content,
-  flavor
+  content :: frame_content(),
+  flavor :: frame_flavor(),
+  point :: true | false
 }).
 
 -record(hls_state, {streamer,
@@ -63,33 +64,33 @@ write_frame(eof, #hls_state{frames = [#hls_frame{dts = Dts}| _] = Frames, dir = 
 
 add_frame(#video_frame{flavor = keyframe, dts = Dts} = Frame, #hls_state{streamer = Streamer, frames = []} = State) ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, start = Dts};
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, last_tables = undefined, start = Dts};
 
 add_frame(#video_frame{flavor = keyframe, dts = Dts} = Frame, #hls_state{streamer = Streamer, start = Start} = State) when Dts - Start < ?DELTA ->
   {NewStreamer, EncFrame} = encode_frame(Streamer#streamer{pat_counter = 0, pmt_counter = 0}, Frame),
-  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, start = Dts};
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, last_tables = undefined, start = Dts};
 
 add_frame(#video_frame{flavor = keyframe, dts = Dts} = Frame, #hls_state{streamer = Streamer, dir = Dir, playlist = Playlist, frames = Frames, last_keyframe = undefined, start = Start, count = N} = State) ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  write(Dir, N, Frames, Playlist, Dts - Start),
-  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, start = Dts, count = N + 1};
+  write(Dir, N, Frames, Playlist, Dts - Start, true),
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame], last_keyframe = Dts, last_tables = undefined, start = Dts, count = N + 1};
 
 add_frame(#video_frame{flavor = keyframe, dts = Dts} = Frame, #hls_state{streamer = Streamer, dir = Dir, playlist = Playlist, duration = Duration, frames = Frames, last_keyframe = LastDts, start = Start, count = N} = State) when Start + Duration - ?DELTA < Dts ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  Pred = fun(#hls_frame{dts = Dts1}) when Dts1 > LastDts -> true;
-    (#hls_frame{dts = Dts1, flavor = keyframe}) when Dts1 == LastDts -> true;
-    (#hls_frame{}) -> false
-  end,
-  {NewFrames, FramesToSegment} = lists:splitwith(Pred, Frames),
+  {NewFrames, FramesToSegment} = split_frames(Frames, LastDts),
   write(Dir, N, FramesToSegment, Playlist, LastDts - Start),
-  State#hls_state{streamer = NewStreamer, frames = [EncFrame|NewFrames], last_keyframe = Dts, start = LastDts, count = N + 1};
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame|NewFrames], last_keyframe = Dts, last_tables = undefined, start = LastDts, count = N + 1};
 
 add_frame(#video_frame{flavor = keyframe, dts = Dts} = Frame, #hls_state{streamer = Streamer, frames = Frames} = State) ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  State#hls_state{streamer = NewStreamer, frames = [EncFrame|Frames], last_keyframe = Dts};
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame|Frames], last_keyframe = Dts, last_tables = undefined};
 
 add_frame(#video_frame{content = video}, #hls_state{last_keyframe = undefined} = State) ->
   State;
+
+add_frame(#video_frame{content = video} = Frame, #hls_state{streamer = Streamer, frames = Frames} = State) ->
+  {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
+  State#hls_state{streamer = NewStreamer, frames = [EncFrame|Frames]};
 
 add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, frames = []} = State) ->
   {NewStreamer, EncFrame} = encode_frame(Streamer#streamer{sent_pat = false}, Frame),
@@ -100,43 +101,55 @@ add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, dir =
   write(Dir, N, Frames, Playlist, Dts - Start),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame], start = Dts, count = N + 1};
 
-add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, frames = Frames, start = Start, duration = Duration} = State) when Start + Duration + ?DELTA < Dts ->
+add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, frames = Frames, last_tables = undefined, start = Start, duration = Duration} = State) when Start + Duration + ?DELTA < Dts ->
   {NewStreamer, EncFrame} = encode_frame(Streamer#streamer{sent_pat = false}, Frame),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame | Frames], last_tables = Dts};
 
 add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, dir = Dir, playlist = Playlist, duration = Duration, frames = Frames, last_keyframe = LastDts, start = Start, count = N} = State) when Start + 3 * Duration / 2 < Dts andalso Start < LastDts andalso Dts - LastDts < Duration ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  Pred = fun(#hls_frame{dts = Dts1}) when Dts1 > LastDts -> true;
-    (#hls_frame{dts = Dts1, flavor = keyframe}) when Dts1 == LastDts -> true;
-    (#hls_frame{}) -> false
-  end,
-  {NewFrames, FramesToSegment} = lists:splitwith(Pred, Frames),
+  {NewFrames, FramesToSegment} = split_frames(Frames, LastDts),
   write(Dir, N, FramesToSegment, Playlist, LastDts - Start),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame|NewFrames], start = LastDts, count = N + 1};
 
 add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, dir = Dir, playlist = Playlist, duration = Duration, frames = Frames, last_keyframe = LastDts, start = Start, count = N} = State) when Start + 3 * Duration / 2 < Dts andalso Start < LastDts andalso Dts - LastDts >= Duration ->
   {NewStreamer, EncFrame} = encode_frame(Streamer#streamer{sent_pat = false}, Frame),
-  Pred = fun(#hls_frame{dts = Dts1}) when Dts1 > LastDts -> true;
-    (#hls_frame{dts = Dts1, flavor = keyframe}) when Dts1 == LastDts -> true;
-    (#hls_frame{}) -> false
-  end,
-  {NewFrames, FramesToSegment} = lists:splitwith(Pred, Frames),
+  {NewFrames, FramesToSegment} = split_frames(Frames, LastDts),
   write(Dir, N, FramesToSegment, Playlist, LastDts - Start),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame|NewFrames], start = LastDts, last_tables = Dts, count = N + 1};
 
 add_frame(#video_frame{dts = Dts} = Frame, #hls_state{streamer = Streamer, dir = Dir, playlist = Playlist, duration = Duration, frames = Frames, last_tables = LastTables, start = Start, count = N} = State) when Start + 3 * Duration / 2 < Dts ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
-  Pred = fun(#hls_frame{dts = Dts1}) when Dts1 > LastTables -> true;
-    (#hls_frame{dts = Dts1, flavor = keyframe}) when Dts1 == LastTables -> true;
-    (#hls_frame{}) -> false
-  end,
-  {NewFrames, FramesToSegment} = lists:splitwith(Pred, Frames),
-  write(Dir, N, FramesToSegment, Playlist, LastTables - Start),
+  {NewFrames, FramesToSegment} = split_frames(Frames, LastTables),
+  write(Dir, N, FramesToSegment, Playlist, LastTables - Start, true),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame|NewFrames], start = LastTables, last_keyframe = undefined, count = N + 1};
 
 add_frame(#video_frame{} = Frame, #hls_state{streamer = Streamer, frames = Frames} = State) ->
   {NewStreamer, EncFrame} = encode_frame(Streamer, Frame),
   State#hls_state{streamer = NewStreamer, frames = [EncFrame|Frames]}.
+
+encode_frame(#streamer{sent_pat = false} = Streamer, #video_frame{content = Content, flavor = Flavor, pts = Pts, dts = Dts} = Frame) ->
+  {NewStreamer, Data} = mpegts:encode(Streamer, Frame),
+  NewFrame = case Data of
+               <<>> ->
+                 undefined;
+               [] ->
+                 undefined;
+               _Else ->
+                 #hls_frame{content = Content, data = Data, flavor = Flavor, pts = Pts, dts = Dts, point = true}
+             end,
+  {NewStreamer, NewFrame};
+
+encode_frame(Streamer, #video_frame{content = video, flavor = keyframe, pts = Pts, dts = Dts} = Frame) ->
+  {NewStreamer, Data} = mpegts:encode(Streamer, Frame),
+  NewFrame = case Data of
+               <<>> ->
+                 undefined;
+               [] ->
+                 undefined;
+               _Else ->
+                 #hls_frame{content = video, data = Data, flavor = keyframe, pts = Pts, dts = Dts, point = true}
+             end,
+  {NewStreamer, NewFrame};
 
 encode_frame(Streamer, #video_frame{content = Content, flavor = Flavor, pts = Pts, dts = Dts} = Frame) ->
   {NewStreamer, Data} = mpegts:encode(Streamer, Frame),
@@ -146,19 +159,40 @@ encode_frame(Streamer, #video_frame{content = Content, flavor = Flavor, pts = Pt
                [] ->
                  undefined;
                _Else ->
-                 #hls_frame{content = Content, data = Data, flavor = Flavor, pts = Pts, dts = Dts}
+                 #hls_frame{content = Content, data = Data, flavor = Flavor, pts = Pts, dts = Dts, point = false}
              end,
   {NewStreamer, NewFrame}.
 
 write(Dir, N, Frames, Playlist, Duration) ->
+  write(Dir, N, Frames, Playlist, Duration, false).
+
+write(Dir, N, Frames, Playlist, Duration, Discontinuity) ->
   FileName = Dir ++ "/stream_" ++ integer_to_list(N) ++ ".ts",
   {ok, File} = file:open(FileName, [write, binary]),
   file:write(File, list_to_binary(lists:reverse([Data || #hls_frame{data = Data} <- Frames]))),
   file:close(File),
-  file:write(Playlist, iolist_to_binary(lists:flatten(io_lib:format("#EXTINF:~.3f,~nstream_~p.ts~n", [Duration / 1000, N])))).
+  case Discontinuity of
+    true ->
+      file:write(Playlist, iolist_to_binary(lists:flatten(io_lib:format("#EXTINF:~.3f,~nstream_~p.ts~n#EXT-X-DISCONTINUITY~n", [Duration / 1000, N]))));
+    _ ->
+      file:write(Playlist, iolist_to_binary(lists:flatten(io_lib:format("#EXTINF:~.3f,~nstream_~p.ts~n", [Duration / 1000, N]))))
+  end.
 
 finish_playlist(Playlist) ->
   file:write(Playlist, iolist_to_binary("#EXT-X-ENDLIST")),
   file:close(Playlist).
+
+split_frames(Frames, Dts) ->
+  split_frames(Frames, Dts, []).
+
+split_frames([#hls_frame{dts = Dts1} = Frame | Frames], Dts, Acc) when Dts1 > Dts ->
+  split_frames(Frames, Dts, [Frame | Acc]);
+
+split_frames([#hls_frame{dts = Dts1, point = false} = Frame | Frames], Dts, Acc) when Dts1 == Dts ->
+  split_frames(Frames, Dts, [Frame | Acc]);
+
+split_frames([#hls_frame{dts = Dts1, point = true} = Frame | Frames], Dts, Acc) when Dts1 == Dts ->
+  {lists:reverse([Frame | Acc]), Frames}.
+
 
 
